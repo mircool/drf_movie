d46b705ca1e3b00af8730570d8a11c94d988cccf
mircool <ynyjyz@163.com>
2024-05-31 10:28:02 +0800

    添加POST方法，实现电影列表的获取与创建。

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -5,8 +6,14 @@ from .models import Movie
 from .serializers import MovieListSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def move_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieListSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieListSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = MovieListSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
