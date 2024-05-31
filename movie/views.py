92be2114ff3a724b13b0079b6262249cb0f2cc49
mircool <ynyjyz@163.com>
2024-05-31 11:16:43 +0800

    重构MovieDetail类，使用generics和mixins简化代码。

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,6 @@
-from django.http import Http404
-from rest_framework import status
+from rest_framework import generics, mixins, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Movie
 from .serializers import MovieDetailSerializer, MovieListSerializer
@@ -21,29 +19,45 @@ def move_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class MovieDetail(APIView):
-    def get(self, request, pk):
-        try:
-            movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            raise Http404
-        serializer = MovieDetailSerializer(movie)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# class MovieDetail(APIView):
+#     def get(self, request, pk):
+#         try:
+#             movie = Movie.objects.get(pk=pk)
+#         except Movie.DoesNotExist:
+#             raise Http404
+#         serializer = MovieDetailSerializer(movie)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#     def put(self, request, pk):
+#         try:
+#             movie = Movie.objects.get(pk=pk)
+#         except Movie.DoesNotExist:
+#             raise Http404
+#         serializer = MovieDetailSerializer(movie, data=request.data, partial=True)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#     def delete(self, request, pk):
+#         try:
+#             movie = Movie.objects.get(pk=pk)
+#         except Movie.DoesNotExist:
+#             raise Http404
+#         movie.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def put(self, request, pk):
-        try:
-            movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            raise Http404
-        serializer = MovieDetailSerializer(movie, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class MovieDetail(generics.GenericAPIView,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin):
+    queryset = Movie.objects.all()
+    serializer_class = MovieDetailSerializer
 
-    def delete(self, request, pk):
-        try:
-            movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            raise Http404
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
