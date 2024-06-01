e74be7c7e1a4791898914b7d1c9c6e2e07609918
mircool <ynyjyz@163.com>
2024-06-01 12:17:47 +0800

    提交信息应为：自定义唯一性验证器，提高用户注册邮箱的校验准确性。

--- account/serializers.py
+++ account/serializers.py
@@ -6,8 +6,14 @@ from rest_framework.validators import UniqueValidator
 User = get_user_model()
 
 
+class CustomUniqueValidator(UniqueValidator):
+    def __call__(self, value, serializer_field):
+        self.message = f'邮箱"{value}"已经被注册'
+        super().__call__(value, serializer_field)
+
+
 # 自定义用户创建序列化器
 class CustomUserCreateSerializer(UserCreateSerializer):
     email = serializers.EmailField(  # 邮箱字段
-        validators=[UniqueValidator(queryset=User.objects.all())]  # 唯一性验证器
+        validators=[CustomUniqueValidator(queryset=User.objects.all())]  # 唯一性验证器
     )
